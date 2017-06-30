600b168701f2bd27bf985de68f1263a10c8ddf5a
Michael DiLeo <Michael.DiLeo@cgb.com>
2017-06-30 12:37:11 -0500

    test checkin

---
+++ GlobalCache/GlobalCache.Tests/Caching/FunctionCacheTests.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using GlobalCache.Caching;
+using System.Runtime.Caching;
+
+namespace GlobalCache.Test.Caching
+{
+    [TestClass]
+    public class FunctionCacheTests
+    {
+        [TestInitialize]
+        public void Setup()
+        {
+            CreateCache();
+        }
+
+
+        [TestMethod]
+        public void caches_function_by_parameters()
+        {
+            FunctionCache cache = CreateCache();
+
+            for (int i = 0; i < 5; i++)
+            {
+                cache.Get(() => i, 0);
+            }
+
+            Assert.AreEqual(1, cache.HitCount.Totals.ExecutionCount);
+        }
+
+        [TestMethod]
+        public void expression_cache_misses_for_different_inputs()
+        {
+            var cache = CreateCache();
+            for (int i = 0; i < 5; i++)
+            {
+                cache.Get(() => i);
+            }
+
+            Assert.AreEqual(5, cache.HitCount.Totals.ExecutionCount);
+        }
+
+        [TestMethod]
+        public void expression_cache_hits_for_same_inputs()
+        {
+            var cache = CreateCache();
+            for (int i = 0; i < 5; i++)
+            {
+                cache.Get(() => 0);
+            }
+
+            Assert.AreEqual(1, cache.HitCount.Totals.ExecutionCount);
+        }
+
+        [TestMethod]
+        public void expression_cache_can_call_another_method_and_use_the_result_value_misses()
+        {
+            var cache = CreateCache();
+            Func<int, int> f = i => i;
+            for (int i = 0; i < 5; i++)
+            {
+                cache.Get(() => f(i));
+            }
+
+            Assert.AreEqual(5, cache.HitCount.Totals.ExecutionCount);
+        }
+
+        [TestMethod]
+        public void expression_cache_can_call_another_method_and_use_the_result_value_hits()
+        {
+            var cache = CreateCache();
+            Func<int, int> f = i => 0;
+            for (int i = 0; i < 5; i++)
+            {
+                cache.Get(() => f(i));
+            }
+
+            Assert.AreEqual(1, cache.HitCount.Totals.ExecutionCount);
+        }
+
+        int testCount = 0;
+        private FunctionCache CreateCache()
+        {
+            return new FunctionCache(new MemoryCache((++testCount).ToString())) { EnableHitCount = true };
+        }
+    }
+}
---
+++ GlobalCache/GlobalCache.Tests/Caching/StoredProcedureCacheTests.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+using Telerik.JustMock;
+using GlobalCache.Caching;
+
+namespace GlobalCache.Test.Caching
+{
+    [TestClass]
+    public class MemCacheTests
+    {
+        private StoredProcedureCache _cache;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            var src = Mock.Create<ISQLSource>();
+            Mock.Arrange(src, s => s.GetSPDataTable(Arg.AnyString, Arg.IsAny<object[]>())).Returns(new DataTable());
+            _cache = new StoredProcedureCache(src);
+            _cache.EnableHitCount = true;
+        }
+
+        [TestMethod]
+        public void cache_hit_for_stored_procedure_keyed_by_parameters()
+        {
+            var proc = "simulated_stored_proc";
+            object[] parameters = new object[] {
+                    "@input1", 0,
+                    "@input2", 1,
+                    "@input3", 2,
+                    "@input4", 3,
+                    "@input5", "",
+                    "@input6", string.Empty,
+                    "@input7", "",
+                    "@input8", string.Empty,
+                    "@input9", 0,
+                    "@input10", 1};
+
+            for (int i = 0; i < 5; i++)
+            {
+                _cache.ExecuteStoredProcedure(proc, parameters);
+            }
+            
+            Assert.AreEqual(1, _cache.HitCount.Totals.ExecutionCount);
+            Assert.AreEqual(5, _cache.HitCount.Totals.RequestCount);
+        }
+
+        [TestMethod]
+        public void correctly_misses_cache()
+        {
+            var proc = "a";
+
+            for (int i = 0; i < 5; i++)
+            {
+                _cache.ExecuteStoredProcedure(proc, i);
+            }
+
+            Assert.AreEqual(5, _cache.HitCount.Totals.ExecutionCount);
+        }
+    }
+}
